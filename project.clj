83199dba0c7b70c6437ff8da9bfb7ef9ff1c5689
Scott Lowe <scottwithtwotees@gmail.com>
2015-01-31 22:35:19 +1100

    Move dev plugin into :dev profile

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[lein-kibit "0.0.8"]]
+  :profiles {:dev {:plugins [[lein-kibit "0.0.8"]]
+                   :env {:dev true}}}
   :main toy-robot.core)
